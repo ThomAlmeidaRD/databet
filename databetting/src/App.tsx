8cdf3a20fd71b1c0acc379d14a4e8d42b74485fb
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-06-08 20:06:27 -0300

    projeto react-ts

---
+++ databetting/src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+
+  function notify(){
+
+  }
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <button onClick={notify}>Notify me!</button>
+    </>
+  )
+}
+
+export default App
