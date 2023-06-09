2b9aa91978cc9f0e2f218bdf05ce9ee99c61dbba
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-06-09 20:03:44 -0300

    homepage

--- databetting/src/main.tsx
+++ databetting/src/main.tsx
@@ -1,6 +1,5 @@
 
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import Home from './pages/Home.tsx'
 import './styles/main.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
@@ -9,10 +8,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 const pages = createBrowserRouter([
     {
         path: '/',
-        element: <App />
-    },
-    {
-        path: '/home',
         element: <Home />
     }
 ])
