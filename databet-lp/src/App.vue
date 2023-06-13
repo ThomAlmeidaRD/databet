9ae6cc79c2c8b4daf8c1aa68076df0b7108d3a13
Thomas Almeida <contato.thomasalmeidard@gmail.com>
2023-06-13 19:12:30 -0300

    setup lp - vue/vite

---
+++ databet-lp/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Emoções Apenas para Comemorar e Dados para Apostar." />
+</template>
---
+++ databet-lp/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+defineProps({
+  msg: String,
+  desc: String
+})
+</script>
+
+<template>
+  
+  <h1>{{ msg }}</h1>
+  
+  <button>
+    Teste Grátis por 7 Dias
+  </button>
+  <button class="link-btn">
+    <a href="#"> O que é o Databett</a>
+  </button>
+
+</template>
+
